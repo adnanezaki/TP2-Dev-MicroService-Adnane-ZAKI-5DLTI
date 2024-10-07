4e50ad5e69a0ac44ab8e43ac72908e1c65f2767d
adnane zaki <93604478+adnanezaki@users.noreply.github.com>
2024-10-07 22:29:00 +0100

    Update AccountMapper.java

--- src/main/java/net/ZAKI/bankaccountservice/mappers/AccountMapper.java
+++ src/main/java/net/ZAKI/bankaccountservice/mappers/AccountMapper.java
@@ -1,8 +1,8 @@
 package net.ZAKI.bankaccountservice.mappers;
 
 import com.fasterxml.jackson.databind.util.BeanUtil;
-import net.elfatehy.bankaccountservice.dto.BankAccountResponseDto;
-import net.elfatehy.bankaccountservice.entities.BankAccount;
+import net.ZAKI.bankaccountservice.dto.BankAccountResponseDto;
+import net.ZAKI.bankaccountservice.entities.BankAccount;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
